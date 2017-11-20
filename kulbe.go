a3230e19d5c14bbb2831df67f987f38a0c4ffc2d
Sebastien Diaz <sebastien.diaz@misys.com>
2017-11-20 16:58:44 +0100

    Add more steps

--- kulbe.go
+++ kulbe.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
-	operator "github.com/enablecloud/kulbe/operators/application"
+	"github.com/derekparker/delve/pkg/config"
+	kubeappoperator "github.com/enablecloud/kulbe/operators/application"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	var eventHandler operator.Handler
-	fmt.Println("hello world")
+	fmt.Println("Start")
+	conf := &config.Config{}
+	var eventHandler kubeappoperator.Handler
+
+	kubeconfig := flag.String("kubeconfig", "", "Path to a kube config. Only required if out-of-cluster.")
+	flag.Parse() // Create the client config. Use kubeconfig if given, otherwise assume in-cluster.
+	config, err := buildConfig(*kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	eventHandler = new(kubeappoperator.Default)
+	eventHandler.Init(conf, kubeappoperator.GetClientOutOfCluster())
+
+	kubeappoperator.Start(conf, config, eventHandler)
+
+}
+
+func buildConfig(kubeconfig string) (*rest.Config, error) {
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	return rest.InClusterConfig()
 }
